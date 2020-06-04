101e8edf21e62dbb5f788c4bf49327ce499a4985
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-04 13:57:50 +0530

    Creating the Store

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,7 @@
-import store from "./store/store";
+import configureStore from "./store/configureStore";
 import * as actions from "./store/bugs";
+
+const store = configureStore();
 
 store.subscribe(() => {
   console.log("Store Changed");
--- my-app/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import reducer from "./bugs";
-
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-); //Higher order Function
-
-export default store;
