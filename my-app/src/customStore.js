c98c91a8cbdb555716a5fd9dbee4e4ac98cf4104
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-04 11:09:24 +0530

    Subscribing to the listners

--- my-app/src/customStore.js
+++ my-app/src/customStore.js
@@ -2,16 +2,25 @@ import reducer from "./reducer";
 
 function createStore(reducer) {
   let state; //Private Property
+  let listners = [];
+
+  function subscribe(listner) {
+    listners.push(listner);
+  }
 
   function dispatch(action) {
     state = reducer(state, action);
+
+    for (let i = 0; i < listners.length; i++) {
+      listners[i]();
+    }
   }
 
   function getState() {
     return state;
   }
 
-  return { getState, dispatch };
+  return { getState, dispatch, subscribe };
 }
 
 export default createStore(reducer);
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,10 @@
 import store from "./customStore";
 import * as actions from "./actions";
 
+store.subscribe(() => {
+  console.log("Store Changed");
+});
+
 store.dispatch(actions.bugAdded("Bug 1"));
 
 console.log(store.getState());
