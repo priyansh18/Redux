d33d74fc272600a56be804d161a2717a72f53971
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-04 12:05:20 +0530

    SEtting up Redux Devtool

--- my-app/src/store.js
+++ my-app/src/store.js
@@ -1,6 +1,9 @@
 import { createStore } from "redux";
 import reducer from "./reducer";
 
-const store = createStore(reducer); //Higher order Function
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); //Higher order Function
 
 export default store;
