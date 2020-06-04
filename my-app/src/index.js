15721ffbeb7871868042e8d4e0b44a63a44f780f
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-04 14:57:22 +0530

    Creating Project Reducer & Store

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,13 @@
 import configureStore from "./store/configureStore";
 import * as actions from "./store/bugs";
+import { projectAdded } from "./store/projects";
 
 const store = configureStore();
 
 store.subscribe(() => {
   console.log("Store Changed");
 });
-
+store.dispatch(projectAdded({ name: "Project 1" }));
 store.dispatch(actions.bugAdded({ description: "Bug 1" }));
 store.dispatch(actions.bugAdded({ description: "Bug 2" }));
 store.dispatch(actions.bugAdded({ description: "Bug 3" }));
