10635967f8a66377b0df32890345466554feff07
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-05 15:07:12 +0530

    Error Message

--- my-app/src/store/configureStore.js
+++ my-app/src/store/configureStore.js
@@ -1,10 +1,11 @@
 import { configureStore,getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from './middleware/logger';
+import toast from './middleware/toast';
 
 export default function () {
   return configureStore({
      reducer,
-     middleware:[...getDefaultMiddleware(),logger('console')]
+     middleware:[...getDefaultMiddleware(),logger('console'),toast]
    });
 }
